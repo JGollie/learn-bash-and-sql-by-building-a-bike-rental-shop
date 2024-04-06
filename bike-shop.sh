7bd62edc5856f783d1120f397e6994ce1b09dbf6
JGollie <jonathan.lgolden@gmail.com>
2024-04-06 19:29:53 -0400

    Save progress: 1570.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -100,7 +100,12 @@ echo "$CUSTOMER_RENTALS"
 if [[ -z $CUSTOMER_RENTALS ]]
 then
 # send to main menu
-MAIN_MENU "You do not have any bikes rented."
+    MAIN_MENU "You do not have any bikes rented."
+else
+# display rented bikes
+# ask for bike to return
+# if not a number
+# send to main menu
 fi
 
 fi
